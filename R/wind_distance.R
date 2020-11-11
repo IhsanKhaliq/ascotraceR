2444e5844500f807a5e5fee33edeea058268406d
IhsanKhaliq <ihsan.khaliq@usq.edu.au>
2020-11-12 08:53:17 +1000

    add wind_speed function

--- R/wind_distance.R
+++ R/wind_distance.R
@@ -1,6 +1,6 @@
 #' Wind distance dispersal parameter
 #'
-#' \code 'wind_distance()' determines distance conidia disperse, in metres, by wind
+#' `wind_distance()` determines distance conidia disperse, in metres, by wind
 #' Conidia are assumed to spread from the centre of each subunit.
 #' The destination subunit, where conidia land, could be the same subunit or another subunit within
 #' or outside the paddock.
@@ -8,12 +8,17 @@
 #' when the wrapping option is chosen.
 #' When the wrapping option is not chosen, the model simulate disease spread within the paddock
 #'
-#' @param average_wind_speed_in_fifteen_minutes A numeric value in m/s
+#' @param average_wind_speed_in_fifteen_minutes A *numeric* value in m/s
 #' @param wind_cauchy_multiplier A Cauchy scaling parameter
 #' @param PSPH A numeric vector estimated from '.estimate_spore_discharge()'
-#' @param return Distance conidia dispersed by wind from the source of infection
-#' @examples  wind_distance(10, PSPH = 10) # output is 10 estimates
-#' wind_distance(10) # output is a single estimate
+#' @return numerical vector, which returns distance conidia dispersed by wind from the source of infection
+#' @examples
+#' @examples
+#' wind_distance(10) # returns a single estimate
+#' wind_distance(10, PSPH = 10) # returns 10 estimates
+#' wind_distance(15, PSPH = c(5, 5)) # returns 10 estimates
+#' @keywords internal
+#' @noRd
 wind_distance <-
   function(average_wind_speed_in_fifteen_minutes,
            wind_cauchy_multiplier = 0.10,
