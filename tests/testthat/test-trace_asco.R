3177ca0cca25265edd67d3be2e245920d8528afd
Paul Melloy <paul.melloy@usq.edu.u>
2020-11-17 09:16:23 +1000

    Initialise test for trace_asco

---
+++ tests/testthat/test-trace_asco.R
@@ -0,0 +1,14 @@
+context("trace_asco is the main function that runs the ascochyta model")
+
+test_that("trace_asco runs without error"){
+  ta1 <- trace_asco(weather = weather_dat,
+                    paddock_length = 100,
+                    paddock_width = 100,
+                    sowing_date =
+                    )
+}
+
+test_that("trace_asco returns an xy data.frame", {
+  expect_is(ta1, "data.frame")
+  expect_true(all(c("x","y") %in% colnames(ta1)))
+})
