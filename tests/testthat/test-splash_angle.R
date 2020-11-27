37aff99fd2cb494ed44050151185547a02507658
IhsanKhaliq <ihsan.khaliq@usq.edu.au>
2020-11-27 12:27:02 +1000

    Added tests for splash angle

--- tests/testthat/test-splash_angle.R
+++ tests/testthat/test-splash_angle.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("The angle of splashed dispersed conidia")
+
+sa1 <- splash_angle()
+
+test_that("splash angle returns a number", {
+  expect_is(sa1, "numeric")
+})
+
+test_that("splash angle is between 1 and 360", {
+  expect_true(sa1 >= 1)
+  expect_true(sa1 <= 360)
 })
