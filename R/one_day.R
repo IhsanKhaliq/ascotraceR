871e67ad8d1e20f65b07cc1397cf5271ef60bbb9
Paul Melloy <paul.melloy@usq.edu.u>
2020-12-18 11:52:08 +1000

    Add Roxygen header, define time increments update daily_vals

---
+++ R/one_day.R
@@ -0,0 +1,37 @@
+#' Simulates ascochyta spore dispersal for a single day increment
+#'
+#' @param i_date
+#' @param daily_vals
+#' @param weather_dat
+#'
+#' @return
+#' @export
+#'
+#' @examples
+one_day <- function(i_date,
+                    daily_vals,
+                    weather_dat){
+
+  # expand time to be hourly
+  i_time <- rep(i_date,24) + lubridate::dhours(0:23)
+
+  # subset weather data by day
+  weather_day <-
+    weather_dat[times %in% i_time,]
+
+  daily_vals <-
+    rbindlist(list(
+      daily_vals,
+      list(daily_vals[.N,ccd]+
+             mean(weather_day[,temp]),
+           daily_vals[.N,cwh] +
+             weather_day[1,wet_hours],
+           daily_vals[.N,cr] +
+             sum(weather_day[,rain], na.rm = TRUE),
+           i_date,
+           yday(i_date)
+                  )
+    ))
+
+  return(daily_vals)
+}
