dc00f196cc2818b254fb0bb53d62a92642e4ed25
Paul Melloy <paul.melloy@usq.edu.u>
2020-12-18 15:23:28 +1000

    create daily_vals_dt and create first test

--- tests/testthat/test-one_day.R
+++ tests/testthat/test-one_day.R
@@ -1,9 +1,19 @@
 context("Simulate disease progression for a single day increment")
 
+# initialise daily_vals
+daily_vals_dt <- data.table::data.table(
+  cdd = 0, # cumulative degree days
+  cwh = 0, # cumulative wet hours
+  cr = 0,  # cumulative rainfall
+  i = as.POSIXct("1998-05-12", tz = "Australia/Perth"),   # day of the simulation (iterator)
+  day = 0  # day of the year
+)
 
-od_t1 <- one_day(day = as.POSIXct("2020-12-18", tz = "UTC"),
-                 )
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+od_t1 <- one_day(i_date = as.POSIXct("1998-05-12", tz = "Australia/Perth"),
+                 daily_vals = daily_vals_dt,
+                 weather_dat = newM_weather)
+
+test_that("one_day returns a data.table", {
+  expect_is(od_t1, "data.table")
 })
