8f7885c58b97299fb01a2565fcfa49a66a5629fc
Paul Melloy <paul.melloy@usq.edu.u>
2020-12-18 17:19:48 +1000

    import weather for test, add test for epidemic relative to sowing date

--- tests/testthat/test-trace_asco.R
+++ tests/testthat/test-trace_asco.R
@@ -1,8 +1,22 @@
 context("trace_asco is the main function that runs the ascochyta model")
 
 # read in example raw data
-weather_dat <- read.csv(file =
+newmarra_raw <- read.csv(file =
                           system.file("extdata", "1998_Newmarracarra_weather_table.csv", package = "Ascotracer"))
+
+weather_dat <- format_weather(
+  x = newmarra_raw,
+  POSIXct_time = "Local.Time",
+  time_zone = "Australia/Perth",
+  temp = "mean_daily_temp",
+  rain = "rain_mm",
+  ws = "ws",
+  wd = "wd",
+  wd_sd = "wd_sd",
+  station = "Location",
+  lat = NA,
+  lon = NA
+)
 
 test_that("trace_asco runs without error" , {
   expect_silent(
@@ -11,12 +25,15 @@ test_that("trace_asco runs without error" , {
       weather = weather_dat,
       paddock_length = 100,
       paddock_width = 100,
+      epidemic_start = "1998-03-10",
       sowing_date = as.POSIXct("1998-03-09"),
-      harvest_date = as.POSIXct("1998-03-12")
+      harvest_date = as.POSIXct("1998-03-12"),
+      time_zone = "Australia/Perth"
     )
 
   )
 })
+
 
 # rerun the first test to make output available as an object to
 #  test in subsequent tests
@@ -24,8 +41,10 @@ ta1 <- trace_asco(
   weather = weather_dat,
   paddock_length = 100,
   paddock_width = 100,
-  sowing_date = "1998-03-09"
-)
+  sowing_date = "1998-03-09",
+  sowing_date = "1998-03-10"
+  )
+
 
 test_that("trace_asco returns an xy data.frame", {
   expect_is(ta1, "data.frame")
@@ -36,3 +55,15 @@ test_that("trace_asco returns the correct data.frame dimensions", {
   expect_equal(nrow(ta1), 10000)
   expect_equal(ncol(ta1), 2)
 })
+
+# Test for stop error is triggered
+test_that("trace_asco stops if epidemic_start is earlier than sowing_start",{
+  ta2 <- trace_asco(
+    weather = weather_dat,
+    paddock_length = 100,
+    paddock_width = 100,
+    sowing_date = "1998-03-09",
+    harvest_date = "1998-03-09"
+  )
+})
+
