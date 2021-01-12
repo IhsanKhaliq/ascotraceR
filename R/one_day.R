b9c307e810fb0d7e3ea037bc78c758e262e8f7f7
Paul Melloy <paul.melloy@usq.edu.u>
2021-01-12 17:40:10 +1000

    Updated oneday description

--- R/one_day.R
+++ R/one_day.R
@@ -1,6 +1,9 @@
 #' Simulates ascochyta spore dispersal for a single day increment
 #'
-#' @param i_date Date (YYYY-MM-DD) for which the dispersal function will iterate for each hour of the day
+#' @param i_date a character string or \class{Date} formated string indicating the
+#'  iteration date of the model. Preferably in \acronym{ISO8601} format (YYYY-MM-DD),
+#'  \emph{e.g.} \dQuote{2020-04-26}.
+#'
 #' @param daily_vals `data.table` of model variable which have been calculated for days prior to the `i_date`
 #' @param weather_dat `data.table` of weather observations which includes the query date `i_date`
 #'
