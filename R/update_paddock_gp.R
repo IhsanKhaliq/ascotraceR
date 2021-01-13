b50596e06d906c112dea995fab6022f4a2f022f3
Paul Melloy <paul.melloy@usq.edu.u>
2021-01-13 14:43:33 +1000

    add parameters and insert roxygen header

--- R/update_paddock_gp.R
+++ R/update_paddock_gp.R
@@ -1,5 +1,20 @@
+#' Title
+#'
+#' @param paddock_coords
+#' @param mean_air_temp
+#' @param mean_air_temp
+#' @param gp_rr
+#' @param max_gp
+#'
+#' @return
+#' @export
+#'
+#' @examples
 update_paddock_gp <- function(paddock_coords,
-                  mean_air_temp){
+                              mean_air_temp,
+                  mean_air_temp,
+                  gp_rr,
+                  max_gp){
   apply(paddock_coords, 1, function(xy){
 
     g_points <- paddockUnifectiveGrowingPoints[xy[1],xy[2]]
