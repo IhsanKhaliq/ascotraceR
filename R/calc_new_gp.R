82733ab4d5b99bb0253ad3f767bcbb9e585cc984
Paul Melloy <paul.melloy@usq.edu.u>
2021-01-13 14:16:21 +1000

    rename new_growing_points function to calc_new_gp

--- R/calc_new_gp.R
+++ R/calc_new_gp.R
@@ -1,8 +1,8 @@
 #' Describes the Growth of Chickpea
 #'
-#' 'new_growing_points()' describes the growth of chickpea in terms of development of growing
+#' 'calc_new_gp()' describes the growth of chickpea in terms of development of growing
 #' points. Upon germination, each germinated seed produces one growing point that multiplies at a rate
-#'that is a function of temperature and is limited by proximity to a maximum growing point density.
+#' that is a function of temperature and is limited by proximity to a maximum growing point density.
 #'
 #' @param current_growing_points Are the current number of growing points at an iteration period
 #' @param gp_rr Chickpea growing points (meristems) replication rate as a
@@ -14,7 +14,7 @@
 #' @example
 #' new_growing_points(current_growing_points = 500, mean_air_temp = 20, gp_rr = 0.0065, max_gp = 15000)
 #'
-new_growing_points <-
+calc_new_gp <-
   function(current_growing_points,
            gp_rr,
            max_gp,
