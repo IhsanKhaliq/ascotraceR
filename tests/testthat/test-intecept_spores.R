4eafd43f0ec4c36b9a2b9c3044a719a011e743d1
Paul Melloy <paul.melloy@usq.edu.u>
2021-01-14 10:20:11 +1000

    initialise new test using default model values

--- tests/testthat/test-intecept_spores.R
+++ tests/testthat/test-intecept_spores.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("Test if the spore interception parameter is calculated approprietly")
+
+test_defaults <- intercept_spores(max_growing_points_limit = 15000,
+                                  max_new_growing_points_limit = 350)
+
+test_that("default values return the correct number",{
+  test_defaults <- intercept_spores(max_growing_points_limit = 15000,
+                                    max_new_growing_points_limit = 350)
+
+  expect_equal(test_defaults, 0.002571429)
+  expect_length(test_defaults,1)
+  expect_is(test_defaults, "numeric")
+
 })
