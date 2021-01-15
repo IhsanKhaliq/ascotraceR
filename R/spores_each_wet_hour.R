6b36899d150a832996fe4fd69ab28fef44fde77f
Paul Melloy <paul.melloy@usq.edu.u>
2021-01-15 11:46:49 +1000

    edit required parameters, create newly_infected vectorspecify loop

--- R/spores_each_wet_hour.R
+++ R/spores_each_wet_hour.R
@@ -1,18 +1,18 @@
 #' Calculates number of conidia dispersed during each wet hour
 #'
-#' 'spores_each_wet_hour()' calcutates number of conidia dispersed during each
+#' 'spores_each_wet_hour()' calculates number of conidia dispersed during each
 #' wet hour when rainfall threshold is reached
 #'
 #' @param h hour in the day which the spores for each wet hour are calculated
 #' @param weather_hourly hourly weather for only the day for which the spores are being estimated
-#' @param infective_elements_list ??
+#' @param paddock data.table of paddock coordinates detailing the growing points and infections at each location
 #' @keywords internal
 #' @noRd
 
 
 spores_each_wet_hour <- function(h,
                                  weather_hourly,
-                                 infective_elements_list) {
+                                 paddock) {
 
   newly_infected_list <- vector(mode = "list")
 
@@ -22,11 +22,14 @@ spores_each_wet_hour <- function(h,
   wind_direction_in_hour = weather_hourly[h, "wd"]
   stdev_wind_direction_in_hour = weather_hourly[h, "wd_sd"]
 
-  for (i_source in 1:length(infective_elements_list)) {
+  newly_infected_list <- vector(length = sum(paddock$infected))
+
+  # this could be changed to an apply function
+  for (i_source in which(paddock$infected)) {
     newly_infected_list <- c(
       newly_infected_list,
       spores_from_1_element(
-        infective_elements_list[[walrus]],
+        paddock[i_source, c("x", "y")],
         rain_in_hour,
         wind_direction_in_hour,
         average_wind_speed_in_hour,
