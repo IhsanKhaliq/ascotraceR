d1452ffd7d1643256866d65f37e298f7c1c5590a
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-15 12:04:40 +1000

    Add extra test with default gps and comments

--- tests/testthat/test-calc_new_gp.R
+++ tests/testthat/test-calc_new_gp.R
@@ -1,5 +1,6 @@
 context("calculates the number of new growing points from an existing number of growing points")
 
+# Test function if current growing points is zero
 test1 <- calc_new_gp(
   current_growing_points = 0,
   gp_rr = 0.0065,
@@ -7,8 +8,17 @@ test1 <- calc_new_gp(
   mean_air_temp = 24
 )
 
+# Test function if current growing points is a negative
 test2 <- calc_new_gp(
   current_growing_points = -1,
+  gp_rr = 0.0065,
+  max_gp = 15000,
+  mean_air_temp = 24
+)
+
+# Test function if current growing points is 40, the model default
+test3 <- calc_new_gp(
+  current_growing_points = 40,
   gp_rr = 0.0065,
   max_gp = 15000,
   mean_air_temp = 24
@@ -17,5 +27,5 @@ test2 <- calc_new_gp(
 test_that("calc_new_gp returns a number",{
   expect_equal(test1, 0)
   expect_equal(round(test2,6), -0.15601)
-
+  expect_equal(round(test3,6), 6.22336)
 })
