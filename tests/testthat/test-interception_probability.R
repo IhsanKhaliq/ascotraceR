a85694e0a65b72a5841f4a60f855c4a5166e445a
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-15 14:24:00 +1000

    initialise interception_probability function

--- tests/testthat/test-interception_probability.R
+++ tests/testthat/test-interception_probability.R
@@ -1,9 +1,17 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+context("Function is use to calculate the max_interception_probability")
 
-IP <- c(500, 5)
+# Define parameters after model defaults
+plant_density <-
+  5 * max(15000)
 
-test_that("the correct values and attributes of a vector is returned", {
-  expect_equal(IP, 40, 40)
+spore_interception_parameter <-
+  0.00006 * (15000 / 350)
+
+test1 <-
+  sapply()
+  interception_probability(target_density = plant_density,
+                           k = spore_interception_parameter)
+
+test_that("function returns the correct probabilities", {
+  expect_equal(test1, 1)
 })
