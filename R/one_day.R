24eb6a983612d5d35e10459dcb2e2dfe98c3941f
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-15 12:34:24 +1000

    add max_new_gp parameters and update spread_spore parameters

--- R/one_day.R
+++ R/one_day.R
@@ -16,6 +16,7 @@ one_day <- function(i_date,
                     weather_dat,
                     gp_rr,
                     max_gp,
+                    max_new_gp,
                     paddock,
                     spore_interception_multiplier) {
 
@@ -60,7 +61,11 @@ one_day <- function(i_date,
   # Update growing points for paddock coordinates
   if(i_wet_hours > 0){
     spread_spores(wet_hours = wet_hours,
-                  weather_hourly = weather_day)
+                  weather_hourly = weather_day,
+                  paddock = paddock,
+                  max_gp =  max_gp,
+                  max_new_gp = max_new_gp,
+                  spore_interception_multiplier = spore_interception_multiplier)
     interception_probability()
   }
 
