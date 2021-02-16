622e1ca051788a012f29a7e049c8de74093fc06f
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-16 13:30:54 +1000

    add NA control flow

--- R/address_from_centre_distance.R
+++ R/address_from_centre_distance.R
@@ -7,10 +7,15 @@
 
 address_from_centre_distance <-
   function(offset_distance, start_address) {
-    as.integer(c(
+    destination <- as.integer(c(
       start_address[1] +
         floor(0.5 + offset_distance[1]),
       start_address[2] +
         floor(0.5 + offset_distance[2])
     ))
+    if(any(is.na(destination))){
+      stop("address_from_centre_distance is returning NAs; check input coordinates\n
+           offset_distance: ",start_address, "start_address: ",start_address)
+    }
+    return(destination)
   }
