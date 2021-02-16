096d42f81c288c933ac2f12fde6d0ded3c7beaeb
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-16 16:49:03 +1000

    initialise test spores_each_wet_hour

--- tests/testthat/test-spores_each_wet_hour.R
+++ tests/testthat/test-spores_each_wet_hour.R
@@ -5,9 +5,20 @@ load("R/sysdata.rda")
 # randomly choose a day to get weather
 day <- sample(unique(newM_weather[newM_weather$wet_hours > 0,times]), size =1)
 
-# I have no idea why this is not working
-# newM_weather[times == day,]
-# which(newM_weather$times == day)
+# makePaddock equivalent
+paddock <- as.data.table(expand.grid(x = 1:paddock_width,
+                                     y = 1:paddock_length))
+
+primary_infection_foci <-
+    paddock[as.integer(round(paddock_width / 2)),
+            as.integer(round(paddock_length / 2))]
+
+# define paddock variables at time 1
+paddock[, new_gp := seeding_rate] # Change in the number of growing points since last iteration
+paddock[, noninfected_gp := seeding_rate] #
+paddock[, infected_gp := fifelse(x == primary_infection_foci[1] &
+                                   y == primary_infection_foci[2], 1,
+                                 0)] # Initialise column of infected growing points
 
 w_dat <- newM_weather[times %in% seq(as.POSIXct("1998-09-02 00:00:00", tz = "Australia/Perth"),
                                      as.POSIXct("1998-09-02 23:00:00", tz = "Australia/Perth"),
