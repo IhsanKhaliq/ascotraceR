0265c9e528af641b220ffc1257f29fc28e1f06b9
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-17 08:41:10 +1000

    stylr; add paddock parameter to spores_each_wethour

--- R/spread_spores.R
+++ R/spread_spores.R
@@ -7,21 +7,24 @@ spread_spores <-
            weather_hourly) {
 
     max_interception_probability <-
-      interception_probability(
-        target_density = 5 * max(paddock$new_gp),
-        k = spore_interception_parameter
-        # k = intercept_spores(
-        #   spore_interception_multiplier = spore_interception_multiplier,
-        #   max_growing_points_limit = max_gp,
-        #   max_new_growing_points_limit = max_new_gp
-        # )
-      )
+      interception_probability(target_density = 5 * max(paddock$new_gp),
+                               k = spore_interception_parameter)
+                               # k = intercept_spores(
+                               #   spore_interception_multiplier = spore_interception_multiplier,
+                               #   max_growing_points_limit = max_gp,
+                               #   max_new_growing_points_limit = max_new_gp
+                               # ))
 
-    for(hour in which(weather_hourly[["rain"]] >= 0.2)){
-      newlyInfectedList <- c(newlyInfectedList,
-                             spores_each_wet_hour(h = hour,
-                                                  weather_hourly = weather_hourly,
-                                                  max_interception_probability = max_interception_probability))
-    }
+                               for (hour in which(weather_hourly[["rain"]] >= 0.2)) {
+                                 newlyInfectedList <- c(
+                                   newlyInfectedList,
+                                   spores_each_wet_hour(
+                                     h = hour,
+                                     weather_hourly = weather_hourly,
+                                     paddock = paddock,
+                                     max_interception_probability = max_interception_probability
+                                   )
+                                 )
+                               }
 
   }
