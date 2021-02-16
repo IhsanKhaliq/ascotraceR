06a1853fd877cca28f80c09b0b93695cfc2f32d9
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-16 14:23:22 +1000

    update test to accommodate data.table output

--- tests/testthat/test-address_from_centre_distance.R
+++ tests/testthat/test-address_from_centre_distance.R
@@ -6,10 +6,13 @@ test1 <- address_from_centre_distance(offset_distance = c(3, 6),
                                       start_address = c(50, 50))
 
 test_that("test1 returns correct coordinates", {
-  expect_equal(test1, c(53, 56))
+  expect_equal(test1, data.table::data.table(x = 53,
+                                             y = 56))
   expect_length(test1, 2)
   expect_true(any(is.na(test1)) == FALSE)
-  expect_type(test1, "integer")
+  expect_type(test1[,x], "integer")
+  expect_type(test1[,y], "integer")
+  expect_is(test1, "data.table")
 })
 
 
@@ -17,10 +20,13 @@ test2 <- address_from_centre_distance(offset_distance = c(-13, 20),
                                       start_address = c(50, 50))
 
 test_that("test4 returns correct coordinates with negatives", {
-  expect_equal(test2, c(37, 70))
+  expect_equal(test2, data.table::data.table(x = 37,
+                                             y = 70))
   expect_length(test2, 2)
   expect_true(any(is.na(test2)) == FALSE)
-  expect_type(test2, "integer")
+  expect_type(test2[,x], "integer")
+  expect_type(test2[,y], "integer")
+  expect_is(test2, "data.table")
 })
 
 test3 <-
@@ -29,10 +35,13 @@ test3 <-
 
 test_that("test3 returns correct coordinates with negatives and floating points",
           {
-            expect_equal(test3, c(round(50 - 45.93146), round(50 - 4.90950)))
+            expect_equal(test3, data.table::data.table(x = round(50 - 45.93146),
+                                                       y = round(50 - 4.90950)))
             expect_length(test3, 2)
             expect_true(any(is.na(test3)) == FALSE)
-            expect_type(test3, "integer")
+            expect_type(test3[,x], "integer")
+            expect_type(test3[,y], "integer")
+            expect_is(test3, "data.table")
           })
 
 test_that("test4 returns an error", {
@@ -41,5 +50,4 @@ test_that("test4 returns an error", {
     start_address = c(50, 50)
   ))
 })
-address_from_centre_distance(offset_distance = c(NA, 5),
-                             start_address = c(50, 50))
+
