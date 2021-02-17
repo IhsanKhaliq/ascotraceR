9bee8f5837ee45d737396c5c9dcab500d0b04f51
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-17 12:25:30 +1000

    Change par spore_packet to spore_targets; correct coordinate assignment

--- R/successful_infections.R
+++ R/successful_infections.R
@@ -3,36 +3,38 @@
 #' 'successful_infections()' determines successful infections. That is, infections
 #'  that have developed into visible lesions
 #'
-#' @param spore_packet a data.table with variables x, y and spores_per_packet
+#' @param spore_targets a data.table with variables x, y and spores_per_packet
+#'  (formally name spore_packet)
 #' @param spores_in_packet indictes??
-#' @param susceptible_growing_points are growing points suceptible to disease
+#' @param susceptible_growing_points are growing points susceptible to disease
 #' @param paddock_new_growing_points indicate??
 #' @param ref_new_growing_points are the number of new growing points
 #' @return successful infections
 #' @keywords internal
 #' @noRd
-successful_infections <- function (spore_packet) {
+successful_infections <- function (spore_targets) {
 
-  address <- c(spore_packet["x"],
-               spore_packet["y"])
+  address <- c(spore_targets["x"],
+               spore_targets["y"])
 
-  spores_in_packet <-  spore_packet["spores_per_packet"]
+  spores_in_packet <-  spore_targets["spores_per_packet"]
 
   susceptible_growing_points <-
-        paddock_new_growing_points[address[1] %% address[2]]
-      if (susceptible_growing_points < -0.5) {
-        susceptible_growing_points <- ref_new_growing_points
-      } else{
-        NULL
-      }
+        paddock[x == spore_targets["x"] &
+                  y == spore_targets["y"], new_gp]
 
-      spores_in_packet <-
-        random_integer_from_real(
-          spores_in_packet * interception_probability(
-            5 * susceptible_growing_points,
-            spore_interception_parameter
-          ) /
-            max_interception_probability
-        )
-      return(list(address, spores_in_packet))
+
+  if (susceptible_growing_points < -0.5) {
+    susceptible_growing_points <- ref_new_growing_points
+  } else{
+    NULL
+  }
+
+  spores_in_packet <-
+    random_integer_from_real(
+      spores_in_packet * interception_probability(5 * susceptible_growing_points,
+                                                  spore_interception_parameter) /
+        max_interception_probability
+    )
+  return(list(address, spores_in_packet))
 }
