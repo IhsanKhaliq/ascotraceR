dd1c079c69b371acf1863b571db4f47b7f893a72
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-17 13:20:42 +1000

    initialise test adjust_for interception

--- tests/testthat/test-adjust_for_interception.R
+++ tests/testthat/test-adjust_for_interception.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+context("adjust coordinates for interception")
+
+library(data.table)
+
+# for manual testing
+spore_dat <- fread("tests/testthat/test-data_adjust_for_interception.csv")
+
+# for automatic check() testing
+#spore_dat <- fread("test-data_adjust_for_interception.csv")
+
+test1 <- adjust_for
