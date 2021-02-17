9b7b473f3efdffeb849cd2a0425b60758c90ba7c
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-17 13:18:12 +1000

    edit so it handles dt of spore packets internally and returns vector
    also update parameters

--- R/successful_infections.R
+++ R/successful_infections.R
@@ -5,32 +5,38 @@
 #'
 #' @param spore_targets a data.table with variables x, y and spores_per_packet
 #'  (formally name spore_packet)
-#' @param spores_in_packet indictes??
-#' @param susceptible_growing_points are growing points susceptible to disease
-#' @param paddock_new_growing_points indicate??
-#' @param ref_new_growing_points are the number of new growing points
-#' @return successful infections
+#' @param paddock data.table containing all parameters for each 1 x 1 coordinate in the paddock
+#' @param spore_interception_parameter
+#' @param max_interception_probability are the number of new growing points
+#' @return a vector of spores_per_packet each referring to a row in spore_targets
 #' @keywords internal
 #' @noRd
-successful_infections <- function (spore_targets) {
+successful_infections <- function (spore_targets,
+                                   paddock,
+                                   spore_interception_parameter,
+                                   max_interception_probability) {
+  suc_inf <-
+    apply(spore_targets,1,function(sp_tar){
 
-  address <- c(spore_targets["x"],
-               spore_targets["y"])
+      address <- c(sp_tar["x"],
+                   sp_tar["y"])
 
-  spores_in_packet <-  spore_targets["spores_per_packet"]
+      spores_in_packet <-  sp_tar["spores_per_packet"]
 
-  susceptible_growing_points <-
-        paddock[x == spore_targets["x"] &
-                  y == spore_targets["y"], new_gp]
+      susceptible_growing_points <-
+        paddock[x == sp_tar["x"] &
+                  y == sp_tar["y"], new_gp]
 
+      spores_in_packet <-
+        random_integer_from_real(
+          spores_in_packet *
+            interception_probability(5 * susceptible_growing_points,
+                                     spore_interception_parameter) /
+            max_interception_probability
+        )
+      return(spores_in_packet)
+    })
 
+  return(c(suc_inf))
 
-
-  spores_in_packet <-
-    random_integer_from_real(
-      spores_in_packet * interception_probability(5 * susceptible_growing_points,
-                                                  spore_interception_parameter) /
-        max_interception_probability
-    )
-  return(list(address, spores_in_packet))
 }
