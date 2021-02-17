349f13184244e80840ee09646ae302ff19921b16
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-17 14:53:03 +1000

    replace square brackets which were wrong

--- R/random_integer_from_real.R
+++ R/random_integer_from_real.R
@@ -7,7 +7,7 @@ random_integer_from_real <- function (r) {
   fraction <- r %% 1
   r_stand <- runif(1)
   fcase(fraction == 0, return(as.integer(r)),
-        r_stand < fraction, return(as.integer(ceiling[r])),
+        r_stand < fraction, return(as.integer(ceiling(r))),
         r_stand >= fraction, return(as.integer(floor(r)))
   )
 }
