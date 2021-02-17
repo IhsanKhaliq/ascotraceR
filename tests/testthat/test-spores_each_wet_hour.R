6b354f2c74f7be3bdc7f03ef037718dd45dc2f77
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-17 16:34:41 +1000

    write test for spores each wet_hour

--- tests/testthat/test-spores_each_wet_hour.R
+++ tests/testthat/test-spores_each_wet_hour.R
@@ -1,7 +1,9 @@
 context("spores spread each day triggered by each wet hour")
 
+load_all()
+
 # load formatted weather data `newM_weather`
-day <- fread("tests/testthat/formatted_weather_one_day.csv")
+weather_day <- fread("tests/testthat/formatted_weather_one_day.csv")
 #day <- fread("formatted_weather_one_day.csv")
 
 # makePaddock equivalent
@@ -14,18 +16,34 @@ primary_infection_foci <-
 # define paddock variables at time 1
 paddock[, new_gp := 40] # Change in the number of growing points since last iteration
 paddock[, noninfected_gp := 40] #
-paddock[, infected_gp := fifelse(x == primary_infection_foci[1] &
-                                   y == primary_infection_foci[2], 1,
+paddock[, infected_gp := fifelse(x >= 53 &
+                                   x <= 57 &
+                                   y >= 53 &
+                                   y <= 57, 5,
                                  0)] # Initialise column of infected growing points
 
 spore_interception_parameter <-
   0.00006 * (15000 / 350)
 
-
-spores_each_wet_hour(
+# write some tests
+set.seed(666)
+test1 <- spores_each_wet_hour(
   h = 1,
-  weather_hourly = day,
+  weather_hourly = weather_day,
   paddock = paddock,
   max_interception_probability = 1,
   spore_interception_parameter = spore_interception_parameter
 )
+
+test_that("test1 returns expected output",{
+  expect_is(test1, "data.table")
+  expect_equal(nrow(test1), 11)
+  expect_equal(colnames(test1), c("x", "y", "spores_per_packet"))
+  expect_equal(test1[1, x], 54)
+  expect_equal(test1[1, y], 53)
+  expect_equal(test1[1, spores_per_packet], 1)
+  expect_is(test1[,x], "integer")
+  expect_is(test1[,y], "integer")
+  expect_is(test1[,spores_per_packet], "integer")
+
+})
