336894957173afb9c18fd8a7338b3937cbcd2c99
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-18 11:15:13 +1000

    Remove any functionality from spread_spores and make obsolete note

--- R/spread_spores.R
+++ R/spread_spores.R
@@ -1,3 +1,5 @@
+# this function has been made redunant as it just passes everything betweeen different levels
+# and does not do anything really important
 spread_spores <-
   function(wet_hours,
            paddock,
@@ -6,9 +8,7 @@ spread_spores <-
            spore_interception_parameter,
            weather_hourly) {
 
-    max_interception_probability <-
-      interception_probability(target_density = 5 * max(paddock$new_gp),
-                               k = spore_interception_parameter)
+
     # k = intercept_spores(
     #   spore_interception_multiplier = spore_interception_multiplier,
     #   max_growing_points_limit = max_gp,
@@ -19,26 +19,7 @@ spread_spores <-
     wet_hour_weather <-
       weather_hourly[rain >= 0.2,]
 
-    newly_infected_list <-
-      lapply(
-        seq_len(nrow(wet_hour_weather)),
-        FUN = spores_each_wet_hour,
-        weather_hourly = weather_day,
-        paddock = paddock,
-        max_interception_probability = max_interception_probability,
-        spore_interception_parameter = spore_interception_parameter
-      )
 
 
-      newlyInfectedList <- c(
-        newlyInfectedList,
-        spores_each_wet_hour(
-          h = hour,
-          weather_hourly = weather_hourly,
-          paddock = paddock,
-          max_interception_probability = max_interception_probability
-        )
-      )
     }
 
-  }
