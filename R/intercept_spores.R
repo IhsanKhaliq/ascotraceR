5f45f51ea5ffb1f7fa8bae742edc0154a883a1cf
IhsanKhaliq <ihsan.khaliq@usq.edu.au>
2021-02-22 15:26:07 +1000

    Rename spore_interception_multiplier to spore_interception_parameter

--- R/intercept_spores.R
+++ R/intercept_spores.R
@@ -5,7 +5,7 @@
 #' The spore_interception_parameter gives an estimate of spores not producing
 #' infections
 #'
-#' @param spore_interception_multiplier multiplier to determine spore interception parameter.
+#' @param spore_interception_parameter multiplier to determine spore interception parameter.
 #'  Defaults to \code{0.00006}
 #' @param max_growing_points_limit Are the maximum number of growing points per square metre.
 #'  Defaults to \code{15000}
@@ -13,9 +13,9 @@
 #' developed per square metre in an iteration period of one day.  Defaults to \code{350}
 #'  @keywords internal
 #'  @noRd
-intercept_spores <- function(spore_interception_multiplier = 0.00006,
+intercept_spores <- function(spore_interception_parameter = 0.00006,
                              max_growing_points_limit,
                              max_new_growing_points_limit) {
-  spore_interception_multiplier * max_growing_points_limit /
+  spore_interception_parameter * max_growing_points_limit /
     max_new_growing_points_limit
 }
