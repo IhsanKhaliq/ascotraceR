9ffd7eb614de289f33290207caa2977454f5b3b8
IhsanKhaliq <ihsan.khaliq@usq.edu.au>
2021-02-22 16:47:54 +1000

    Add working tests for maximum growing points function

--- tests/testthat/test-max_growing_points.R
+++ tests/testthat/test-max_growing_points.R
@@ -1,3 +1,10 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context ("maximum growing points")
+
+
+max_gp <- 15000 * (1 - exp(-0.138629 * 40))
+
+test_that("correct numbers are returned", {
+  expect_length(max_gp, 1)
+  expect_is(max_gp, "numeric")
+  expect_equivalent(max_gp, 14941.4052278536)
 })
