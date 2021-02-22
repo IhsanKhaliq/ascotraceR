6216ff3301f69c21dbf08f067b680717cffb1753
IhsanKhaliq <ihsan.khaliq@usq.edu.au>
2021-02-23 09:48:34 +1000

    Add tests for the per_area function

--- tests/testthat/test-per_area.R
+++ tests/testthat/test-per_area.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("per_area")
+
+test_1 <- 1*(1* 20*20)
+
+
+test_that("correct parameter values are returned", {
+  expect_equal(test_1, 400)
+  expect_length(test_1, 1)
+  expect_type(test_1, "double")
+  expect_is(as.integer(test_1), "integer")
 })
