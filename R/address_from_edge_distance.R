5abf7f8a33dc988ffe814b192705ef210acb0b25
IhsanKhaliq <ihsan.khaliq@usq.edu.au>
2021-02-22 11:25:55 +1000

    Fix typos in address from edge distance function

--- R/address_from_edge_distance.R
+++ R/address_from_edge_distance.R
@@ -3,8 +3,8 @@
 #' 'address_from_edge_distance()' gives information on the possible destination
 #' where conidia may land when dispersed by wind driven rain or rain splash
 #'
-#' @param summary_unit_width is the width of the observation quadrat/cell, thais is 1 square metre
-#' @param summary_unit_length is the length of the observation quadrat/cell, that is 1 square metre
+#' @param summary_unit_width is the width of the observation quadrat/cell, which is equal to 1 square metre
+#' @param summary_unit_length is the length of the observation quadrat/cell, which is equal to 1 square metre
 #' @param location[1] represents origin of dispersal
 #' @param location[2] represents the destination where conidia land
 #' @return distance in metres
