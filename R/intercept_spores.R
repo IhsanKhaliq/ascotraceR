9791fd4e17439da36348f6a60870d4f90264da4d
IhsanKhaliq <ihsan.khaliq@usq.edu.au>
2021-02-22 12:02:08 +1000

    Add values to spore interception function parameter

--- R/intercept_spores.R
+++ R/intercept_spores.R
@@ -7,8 +7,10 @@
 #'
 #' @param spore_interception_multiplier multiplier to determine spore interception parameter.
 #'  Defaults to \code{0.00006}
-#' @param max_growing_points_limit Are the maximum number of growing points chickpea can achieve
-#' @param max_new_growing_points_limit A *numeric* number giving number of new growing points
+#' @param max_growing_points_limit Are the maximum number of growing points per square metre.
+#'  Defaults to \code{15000}
+#' @param max_new_growing_points_limit A *numeric* number indicating number of new growing points
+#' developed per square metre in an iteration period of one day.  Defaults to \code{350}
 #'  @keywords internal
 #'  @noRd
 intercept_spores <- function(spore_interception_multiplier = 0.00006,
