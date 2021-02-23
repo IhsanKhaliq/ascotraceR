2c01b714dd1679923f45e764d62494a8589cdbd0
IhsanKhaliq <ihsan.khaliq@usq.edu.au>
2021-02-23 14:45:32 +1000

    Add tests for the summary unit area function

--- tests/testthat/test-summary_unit_area.R
+++ tests/testthat/test-summary_unit_area.R
@@ -1,3 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("summary unit area")
+
+test_1 <- (20 * 20)
+
+
+test_that("correct values are returned when summary unit is a paddock", {
+  expect_equal(test_1, 400)
+  expect_length(test_1, 1)
+  expect_type(test_1, "double")
+  expect_is(as.integer(test_1), "integer")
+})
+
+test_2 <- (1 * 1)
+
+test_that("correct values are returned when summary unit is an observation quadrat", {
+  expect_equal(test_2, 1)
+  expect_length(test_2, 1)
+  expect_type(test_2, "double")
+  expect_is(as.integer(test_2), "integer")
 })
