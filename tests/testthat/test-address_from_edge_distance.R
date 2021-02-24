82f316b9b72fb47b860fbf3614e18688538e1f56
IhsanKhaliq <ihsan.khaliq@usq.edu.au>
2021-02-24 10:21:13 +1000

    ""

--- tests/testthat/test-address_from_edge_distance.R
+++ tests/testthat/test-address_from_edge_distance.R
@@ -7,5 +7,5 @@ test_1 <- c(1 + floor(1 / 1), (1 + floor(1 / 1)))
 test_that("multiplication works", {
   expect_length(test_1, 2)
   expect_true(any(is.na(test_1)) == FALSE)
-  expect_is(is.integer(test_1), "integer")
+
 })
