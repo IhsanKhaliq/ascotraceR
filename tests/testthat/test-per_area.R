dfad709d358b3594790e7ab49c13f467fedb6497
IhsanKhaliq <ihsan.khaliq@usq.edu.au>
2021-02-24 12:03:03 +1000

    Modify per_aerea test

--- tests/testthat/test-per_area.R
+++ tests/testthat/test-per_area.R
@@ -1,11 +1,10 @@
 context("per_area")
 
-test_1 <- 1*(1* 20*20)
+test_1 <- list(1* 20*20)
 
 
 test_that("correct parameter values are returned", {
-  expect_equal(test_1, 400)
+  expect_equivalent(test_1, 400)
   expect_length(test_1, 1)
-  expect_type(test_1, "double")
   expect_is(as.integer(test_1), "integer")
 })
