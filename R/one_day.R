460cc7d11ec7bedc3a52ebe15a6dd1db47519ea9
Paul Melloy <paul.melloy@usq.edu.u>
2021-02-25 17:56:41 +1000

    Reorder function and stylr

--- R/one_day.R
+++ R/one_day.R
@@ -54,20 +54,27 @@ one_day <- function(i_date,
                                k = spore_interception_parameter)
 
 
-    newly_infected_list <-
-      lapply(
-        seq_len(nrow(weather_day[rain >= 0.1,])),
-        FUN = spores_each_wet_hour,
-        weather_hourly = weather_day[rain >= 0.2,],
-        paddock = daily_vals[["paddock"]],
-        max_interception_probability = max_interception_probability,
-        spore_interception_parameter = spore_interception_parameter
+    newly_infected_dt <-
+      rbindlist(
+        lapply(
+          seq_len(nrow(weather_day[rain >= 0.1, ])),
+          FUN = spores_each_wet_hour,
+          weather_hourly = weather_day[rain >= 0.2, ],
+          paddock = daily_vals[["paddock"]],
+          max_interception_probability = max_interception_probability,
+          spore_interception_parameter = spore_interception_parameter
+        )
       )
+    newly_infected_dt[, cdd_at_infection := daily_vals[["cdd"]]]
 
-    newly_infected_list <- rbindlist(newly_infected_list)
+    daily_vals[["newly_infected"]] <- rbind(daily_vals[["newly_infected"]],
+                                            newly_infected_dt)
 
-    # daily_vals[["paddock"]][x == spore_packet["x"] &
-    #   y == spore_packet["y"], infective_element := 1]
+    daily_vals <- make_some_infective(daily_vals = daily_vals,
+                                      latent_period = 200)
+
+    #newly_infected_list <- rbindlist(newly_infected_list)
+
 
     # newlyInfectedListList is list(ccd = ccd, newlyInfectedList)
     # additionalNewlyInfectedList - seems to be only the coordinates give at the start of the model ie (primary_infection_foci)
@@ -79,7 +86,18 @@ one_day <- function(i_date,
              new = c("x","y","spores_per_packet")
               )
 
-    daily_vals[[paddock]]
+    daily_vals <-
+      apply(newly_infected_list, 1, function(NIL, DVL = daily_vals) {
+        DVL[[paddock]][x == NIL["x"] &
+                         y == NIL["x"], c("infected_gp", "cdd_at_infection") :=
+                         list(NIL["spores_per_packet"],
+                              DVL[["cdd"]])]
+
+        return(DVL)
+
+      })
+
+    daily_vals[[paddock]][,]
   }
 
 
