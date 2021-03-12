5203edf6d4455f17b80a9324f8bad0624e4819be
Paul Melloy <paul.melloy@usq.edu.u>
2021-03-12 22:12:18 +1000

    add 'sf' package as import

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,11 @@
 #' @import data.table
 NULL
+
+# This function is never called.
+# It only makes `sf` available in the package for `lutz` to use.
+# `lutz` only has `sf` in Suggests, but it is needed for the function we use
+# from `lutz`
+
+stub <- function() {
+  sf::st_as_sf()
+}
