b7f62e7a3fe78b3e74e253abf6a1d5f795190044
IhsanKhaliq <54728693+IhsanKhaliq@users.noreply.github.com>
2021-07-22 09:20:26 +1000

    Change primary infection intensity from 40 to 100 & hash out function that relates infection intensity to seeding rate, as they have no relations

--- R/trace_asco.R
+++ R/trace_asco.R
@@ -131,7 +131,7 @@
 #'   gp_rr = 0.0065,
 #'   spores_per_gp_per_wet_hour = 0.22,
 #'   latent_period_cdd = 150,
-#'   primary_infection_intensity = 40)
+#'   primary_infection_intensity = 100)
 #'   traced[[145]]
 #'
 #'   tracer_plot(dat = traced,
@@ -183,12 +183,12 @@ trace_asco <- function(weather,
   }
 
 
-  if (primary_infection_intensity > seeding_rate) {
-    stop(
-      "primary_infection_intensity exceeds the number of starting growing points - 'seeding_rate': ",
-      seeding_rate
-    )
-  }
+  # if (primary_infection_intensity > seeding_rate) {
+  #   stop(
+  #     "primary_infection_intensity exceeds the number of starting growing points - 'seeding_rate': ",
+  #     seeding_rate
+  #   )
+  # }
 
   # convert times to POSIXct -----------------------------------------------
   initial_infection <-
