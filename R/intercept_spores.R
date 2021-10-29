14b3349d2d16acaff07d6378dda47d152d1656a1
IhsanKhaliq <54728693+IhsanKhaliq@users.noreply.github.com>
2021-10-30 09:02:09 +1000

    Re-flow comment

--- R/intercept_spores.R
+++ R/intercept_spores.R
@@ -1,19 +1,19 @@
 #' Calculate spore interception parameter
 #'
 #' The parameter which controls the relationship between uninfected growing
-#' points per area and probability of a spore causing an infection.
-#' The spore_interception_parameter gives an estimate of spores not producing
+#' points per area and probability of a spore causing an infection. The
+#' spore_interception_parameter gives an estimate of spores not producing
 #' infections
 #'
 #' @param spore_interception_parameter multiplier to determine spore
-#' interception parameter. Defaults to \code{0.00006}
+#'   interception parameter. Defaults to \code{0.00006}
 #' @param max_growing_points_limit Are the maximum number of growing points per
-#'  square metre. efaults to \code{15000}
+#'   square metre. efaults to \code{15000}
 #' @param max_new_growing_points_limit A *numeric* number indicating number of
-#'  new growing points developed per square metre in an iteration period of one
-#'  day.  Defaults to \code{350}
-#'  @keywords internal
-#'  @noRd
+#'   new growing points developed per square metre in an iteration period of one
+#'   day.  Defaults to \code{350}
+#' @keywords internal
+#' @noRd
 intercept_spores <- function(spore_interception_parameter = 0.00006,
                              max_growing_points_limit,
                              max_new_growing_points_limit) {
