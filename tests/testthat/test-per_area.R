40171ffe88e99a70b1dcf14bacc444a6f799a88e
IhsanKhaliq <54728693+IhsanKhaliq@users.noreply.github.com>
2021-10-30 11:44:28 +1000

    Delete test for per area function

--- tests/testthat/test-per_area.R
+++
@@ -1,9 +0,0 @@
-
-test_1 <- list(1* 20*20)
-
-
-test_that("correct parameter values are returned", {
-  expect_equivalent(test_1, 400)
-  expect_length(test_1, 1)
-  expect_is(as.integer(test_1), "integer")
-})
