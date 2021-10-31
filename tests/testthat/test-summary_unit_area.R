930c0860844fda034d91cd764286c3fb533fa4aa
IhsanKhaliq <54728693+IhsanKhaliq@users.noreply.github.com>
2021-11-01 09:21:24 +1000

    Add tests for calculating the area of a summary unit and paddock in the summary unit area function

--- tests/testthat/test-summary_unit_area.R
+++ tests/testthat/test-summary_unit_area.R
@@ -5,6 +5,7 @@ test_that("correct values are returned when summary unit is a paddock", {
   expect_length(test_1, 1)
   expect_type(test_1, "double")
   expect_is(as.integer(test_1), "integer")
+  expect_equal(20*20, 400)
 })
 
 test_2 <- (1 * 1)
@@ -15,4 +16,5 @@ test_that("correct values are returned when summary unit is an observation
   expect_length(test_2, 1)
   expect_type(test_2, "double")
   expect_is(as.integer(test_2), "integer")
+  expect_equal(1*1, 1)
 })
