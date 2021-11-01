00e9a596dbea6c95f213d1ac8fd6c9b41df7d490
IhsanKhaliq <54728693+IhsanKhaliq@users.noreply.github.com>
2021-11-01 10:54:10 +1000

    Update paddock dimension tests

--- tests/testthat/test-paddock_dimention.R
+++ tests/testthat/test-paddock_dimention.R
@@ -1,13 +1,11 @@
-# this doesn't test any functionality from this package that I can tell? -as
-# it needs to be rewritten to actually test the paddock_dimension(), this just
-# tests `ceiling()`
+test_1 <-  ceiling(20 / 1)
+ceiling(20 / 1)
 
-test_1 <-  ceiling(200 / 80)
-ceiling(200 / 80)
-
-test_that("correct parameters values are returned", {
-  expect_equal(test_1, 3)
+test_that("correct parameters values and paddock dimensions are returned", {
+  expect_equal(test_1, 20)
   expect_length(test_1, 1)
   expect_type(test_1, "double")
   expect_is(as.integer(test_1), "integer")
+  expect_true(any(is.na(test_1)) == FALSE)
 })
+
