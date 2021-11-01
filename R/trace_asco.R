fc65a3d88759a892304f42bddce72411aa3e8d0d
Adam Sparks <adamhsparks@gmail.com>
2021-11-01 15:07:44 +0800

    correct some grammar in documentation

--- R/trace_asco.R
+++ R/trace_asco.R
@@ -22,7 +22,7 @@
 #'   Celsius per day. Defaults to `0.0065`.
 #' @param max_gp_lim Maximum number of chickpea growing points (meristems)
 #'   allowed per square metre. Defaults to `15000`.
-#' @param max_new_gp Maximum number of new chickpea growing points (meristems)
+#' @param max_new_gp Maximum number of new chickpea growing points (meristems),
 #'   which develop per day, per square metre. Defaults to `350`.
 #' @param primary_infection_foci refers to the inoculated coordinates where the
 #'   epidemic starts. Accepted inputs are: `centre` (Default) or `random`.
