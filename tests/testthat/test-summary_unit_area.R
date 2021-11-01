1b55abba4b4892da4ae5593334bb424a7228eef2
IhsanKhaliq <54728693+IhsanKhaliq@users.noreply.github.com>
2021-11-01 10:55:58 +1000

    Remove tests for paddock. Summary unit area represent observation quadrat in the model, not paddock

--- tests/testthat/test-summary_unit_area.R
+++ tests/testthat/test-summary_unit_area.R
@@ -1,20 +1,10 @@
-test_1 <- (20 * 20)
-
-test_that("correct values are returned when summary unit is a paddock", {
-  expect_equal(test_1, 400)
-  expect_length(test_1, 1)
-  expect_type(test_1, "double")
-  expect_is(as.integer(test_1), "integer")
-  expect_equal(20*20, 400)
-})
-
-test_2 <- (1 * 1)
+test_1 <- (1 * 1)
 
 test_that("correct values are returned when summary unit is an observation
           quadrat", {
-  expect_equal(test_2, 1)
-  expect_length(test_2, 1)
-  expect_type(test_2, "double")
-  expect_is(as.integer(test_2), "integer")
+  expect_equal(test_1, 1)
+  expect_length(test_1, 1)
+  expect_type(test_1, "double")
+  expect_is(as.integer(test_1), "integer")
   expect_equal(1*1, 1)
 })
