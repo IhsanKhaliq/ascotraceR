0ff39b13422ef06decfd90428567f3ce4821c614
Adam Sparks <adamhsparks@gmail.com>
2021-11-03 12:11:15 +0800

    Format code and use sum() in place of `+` clarify error response

--- R/address_from_centre_distance.R
+++ R/address_from_centre_distance.R
@@ -6,27 +6,34 @@
 #' @keywords internal
 #' @noRd
 
-
 address_from_centre_distance <-
   function(offset_distance, start_address) {
-    destination <- as.integer(c(
-      start_address[1] +
-        floor(0.5 + offset_distance[1]),
-      start_address[2] +
-        floor(0.5 + offset_distance[2])
-    ))
+    destination <- as.integer(c(sum(start_address[1],
+                                    floor(
+                                      sum(0.5, offset_distance[1])
+                                    )),
+                                sum(start_address[2],
+                                    floor(
+                                      sum(0.5, offset_distance[2])
+                                    ))))
     if (any(is.na(destination))) {
       stop(
         call. = FALSE,
-        "address_from_centre_distance is returning NAs;",
-        " check input coordinates\n",
-        "offset_distance: ", offset_distance[1], ", ", offset_distance[2],
-        "; start_address: ", start_address[1], ", ",  start_address[2]
+        "`address_from_centre_distance()` is returning `NA`; ",
+        "check input coordinates\n",
+        "offset_distance: ",
+        offset_distance[1],
+        ", ",
+        offset_distance[2],
+        "; start_address: ",
+        start_address[1],
+        ", ",
+        start_address[2]
       )
     }
 
-    # make destination a data table
+    # make destination a data.table
     destination <- data.table(x = destination[1],
-                                          y = destination[2])
+                              y = destination[2])
     return(destination)
   }
