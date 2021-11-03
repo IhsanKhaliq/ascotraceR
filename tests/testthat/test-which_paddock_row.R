a65cd0d961ec6ba4e55d76e2d38416e4c7c6a196
IhsanKhaliq <54728693+IhsanKhaliq@users.noreply.github.com>
2021-11-03 10:01:45 +1000

    Write tests for which paddock row function

---
+++ tests/testthat/test-which_paddock_row.R
@@ -0,0 +1,15 @@
+pdk <- as.data.table(expand.grid(x = 1:100,
+                                 y = 1:100))
+qry <- pdk[sample(1:nrow(pdk), 5),]
+which_paddock_row(pdk, qry)
+
+test1 <- which_paddock_row(pdk, qry)
+
+test_that("test1 gives correct output", {
+  expect_false(any(is.na(test1)))
+  expect_length(test1, 5)
+  expect_true(is.vector(test1))
+  expect_equal(max(test1), 9667)
+  expect_equal(min(test1), 2601)
+  expect_is(test1, "integer")
+})
