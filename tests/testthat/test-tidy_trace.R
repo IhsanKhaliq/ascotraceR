968e946f09daae448036b273b3a4ead1a5edb744
Adam Sparks <adamhsparks@gmail.com>
2021-11-06 21:03:57 +0800

    Add tests for summarise_trace() and tidy_trace()

---
+++ tests/testthat/test-tidy_trace.R
@@ -0,0 +1,36 @@
+tidy_test <- tidy_trace(
+  trace_asco(
+    weather = newM_weather,
+    paddock_length = 100,
+    paddock_width = 100,
+    initial_infection = "1998-05-10",
+    sowing_date = as.POSIXct("1998-05-09"),
+    harvest_date = as.POSIXct("1998-05-12"),
+    time_zone = "Australia/Perth",
+    primary_infection_foci = "centre"
+  )
+)
+
+test_that("tidy_trace() produces expected output", {
+  expect_s3_class(tidy_test, c("data.table", "data.frame"))
+  expect_equal(length(tidy_test), 13)
+  expect_equal(nrow(tidy_test), 50000)
+  expect_named(
+    tidy_test,
+    c(
+      "i_day",
+      "i_date",
+      "day",
+      "x",
+      "y",
+      "new_gp",
+      "susceptible_gp",
+      "exposed_gp",
+      "infectious_gp",
+      "cdd",
+      "cwh",
+      "cr",
+      "gp_standard"
+    )
+  )
+})
