18e4c3370404144f5a4cdc7f565c4a2813b0784d
Adam Sparks <adamhsparks@gmail.com>
2021-11-11 09:40:16 +0800

    Add "summarize" spelling version of summarise_trace

--- R/summarise_trace.R
+++ R/summarise_trace.R
@@ -145,3 +145,38 @@ summarise_trace <- function(trace) {
 
   return(sum(infprod))
 }
+
+#' @rdname summarise_trace
+#' @examplesIf interactive()
+#'
+#' Newmarracarra <-
+#'    read.csv(system.file("extdata",
+#'             "1998_Newmarracarra_weather_table.csv", package = "ascotraceR"))
+#' station_data <-
+#'    system.file("extdata", "stat_dat.csv", package = "ascotraceR")
+#'
+#' weather_dat <- format_weather(
+#'    x = Newmarracarra,
+#'    POSIXct_time = "Local.Time",
+#'    temp = "mean_daily_temp",
+#'    ws = "ws",
+#'    wd_sd = "wd_sd",
+#'    rain = "rain_mm",
+#'    wd = "wd",
+#'    station = "Location",
+#'    time_zone = "Australia/Perth",
+#'    lonlat_file = station_data)
+#'
+#' traced <- trace_asco(
+#'   weather = weather_dat,
+#'   paddock_length = 100,
+#'   paddock_width = 100,
+#'   initial_infection = "1998-06-10",
+#'   sowing_date = as.POSIXct("1998-06-09"),
+#'   harvest_date = as.POSIXct("1998-06-09") + lubridate::ddays(100),
+#'   time_zone = "Australia/Perth",
+#'   primary_infection_foci = "centre")
+#'
+#' summarised <- summarise_trace(traced)
+#' @export
+summarize_trace <- summarise_trace
