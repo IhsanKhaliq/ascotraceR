dc12af6f227cd157dcf65c3e33de2fb07eea575f
PaulMelloy <paul@melloy.com.au>
2021-11-15 07:25:06 +1000

    Add more weather checks

--- R/format_weather.R
+++ R/format_weather.R
@@ -573,4 +573,45 @@ format_weather <- function(x,
                                             "Temperature inputs are outside expected ranges (-30 and +60 degrees C); \n",
                                             paste(as.character(final_w[temp < -30 | temp > 60,times])),
                                             "\nplease correct these inputs and run again")
+
+  # Check rainfall
+  # For NAs
+  if(nrow(final_w[is.na(rain),]) != 0) stop(call. = FALSE,
+                                            "NA values in rainfall; \n",
+                                            paste(as.character(final_w[is.na(rain),times])),
+                                            "\nplease correct these inputs and run again")
+  # for outside range
+  if(nrow(final_w[rain < 0 |
+                  rain > 100,]) != 0) stop(call. = FALSE,
+                                          "rain inputs are outside expected ranges (0 and 100 mm); \n",
+                                          paste(as.character(final_w[rain < 0 | rain > 100,times])),
+                                          "\nplease correct these inputs and run again")
+
+  # Check windspeed
+  # For NAs
+  if(nrow(final_w[is.na(ws),]) != 0) stop(call. = FALSE,
+                                          "NA values in wind speed; \n",
+                                          paste(as.character(final_w[is.na(ws),times])),
+                                          "\nplease correct these inputs and run again")
+  # for outside range
+  if(nrow(final_w[ws < 0 |
+                  ws > 150,]) != 0) stop(call. = FALSE,
+                                           "wind speed inputs are outside expected ranges (0 and 150 kph); \n",
+                                           paste(as.character(final_w[ws < 0 | ws > 150,times])),
+                                           "\nplease correct these inputs and run again")
+
+  # Check Wind direction
+  # For NAs
+  if(nrow(final_w[is.na(wd),]) != 0) stop(call. = FALSE,
+                                            "NA values in wind direction; \n",
+                                            paste(as.character(final_w[is.na(wd),times])),
+                                            "\nplease correct these inputs and run again")
+  # for outside range
+  if(nrow(final_w[wd < 0 |
+                  wd > 360,]) != 0) stop(call. = FALSE,
+                                           "wind direction are outside expected ranges (0 and 360); \n",
+                                           paste(as.character(final_w[wd < 0 | rain > 360,times])),
+                                           "\nplease correct these inputs and run again")
+
+
 }
