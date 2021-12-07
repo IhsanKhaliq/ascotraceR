4b131827747ff112fdb487d1d8bed618bfd7a469
Adam Sparks <adamhsparks@gmail.com>
2021-12-07 17:27:15 +0800

    clarify docs, remove redundant code

--- R/which_paddock_row.R
+++ R/which_paddock_row.R
@@ -1,7 +1,7 @@
-#' Returns the row numbers of an x y data frame
+#' Returns the row numbers of an x y data.table
 #'
-#' @param paddock The template data frame you wish to know the row numbers of
-#' @param query Data.frame with column names 'x' and 'y' for which you want to
+#' @param paddock The template data.table you wish to know the row numbers of
+#' @param query data.frame with column names 'x' and 'y' for which you want to
 #'   know the row number of in the paddock data.table
 #'
 #' @return Vector of row numbers
@@ -9,25 +9,19 @@
 #' @examples
 #' pdk <- data.table(CJ(x = 1:100,
 #'                      y = 1:100))
-#' qry <- pdk[sample(1:nrow(pdk), 5),]
-#' which_paddock_row(pdk, qry)
+#' qry <- pdk[sample(1:nrow(pdk), 5), ]
+#' which_paddock_row(paddock = pdk, query = qry)
 #' @keywords internal
 #' @noRd
 which_paddock_row <- function(paddock, query){
   x <- y <- NULL
 
-  setDT(paddock)
-  rows1 <-
-    apply(query, 1, function(qu){
-
+  rows1 <- apply(query, 1, function(qu){
       y_max <- paddock[, max(y)]
       x_max <- paddock[, max(x)]
-
       x_rows <- qu["x"]
       y_rows <- (qu["y"] * x_max) - x_max
-
       return(x_rows + y_rows)
-
     })
 
   return(unlist(rows1))
