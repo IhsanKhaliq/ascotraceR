1660da0ea5e13e3e5f96edafcad09cca00d001b1
IhsanKhaliq <54728693+IhsanKhaliq@users.noreply.github.com>
2021-12-15 09:20:31 +1000

    Change default value from 10 to 1. We had maximum three quadrats infected at the time of initial infection

--- R/trace_asco.R
+++ R/trace_asco.R
@@ -38,7 +38,7 @@
 #'   as lesions on chickpea plants at the time of `initial_infection`. On the
 #'   date of initial infection in the experiment, one infectious growing point
 #'   was added to each of the sub-unit corresponding to the point of initial
-#'   infection in the field. Defaults to `10`.
+#'   infection in the field. Defaults to `1`.
 #' @param n_foci refers to number of inoculated quadrats (with primary
 #'   inoculum). The value is always `1` if `primary_infection_foci = "centre"`.
 #'   The value can be greater than `1` if `primary_infection_foci = "random"`.
