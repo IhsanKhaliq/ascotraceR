22cb981d57446b55bf011b31989592872af20503
IhsanKhaliq <54728693+IhsanKhaliq@users.noreply.github.com>
2021-12-15 09:28:44 +1000

    Change the description of primary infection intensity parameter

--- R/trace_asco.R
+++ R/trace_asco.R
@@ -34,13 +34,13 @@
 #'   column is optional and can specify the `primary_infection_intensity` for
 #'   each coordinate. If \sQuote{load} is not specified
 #'   `primary_infection_intensity` is substituted for all coordinates.
-#' @param primary_infection_intensity Refers to the amount of primary infection
-#'   as lesions on chickpea plants at the time of `initial_infection`. On the
+#' @param primary_infection_intensity Refers to the number of quadrats where
+#' infection is observed at the time of `initial_infection`. On the
 #'   date of initial infection in the experiment, one infectious growing point
-#'   was added to each of the sub-unit corresponding to the point of initial
+#'   is added to each of the sub-unit corresponding to the point of initial
 #'   infection in the field. Defaults to `1`.
 #' @param n_foci refers to number of inoculated quadrats (with primary
-#'   inoculum). The value is always `1` if `primary_infection_foci = "centre"`.
+#'   inoculum) for initiating infection. The value is always `1` if `primary_infection_foci = "centre"`.
 #'   The value can be greater than `1` if `primary_infection_foci = "random"`.
 #' @param stubble_inoculum_coords refers to the inoculated coordinates where the
 #'   crop stubble/debris are likely to contribute to splash dispersal of the
